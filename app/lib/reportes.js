c968dd37fe68f037d7846e86edf38bd619950453
MateoMigone <mateomigone01@gmail.com>
2025-01-03 19:52:17 -0300

    token log

--- app/lib/reportes.js
+++ app/lib/reportes.js
@@ -4,7 +4,7 @@ export const downloadPdf = async (obraId, gremioId, gremioName) => {
   try {
     const auth = getAuth(); // Ensure Firebase Authentication is initialized
     const token = await auth.currentUser.getIdToken(); // Get the user's ID token
-    console.log("token: ", token);
+    console.log("tokennn: ", token);
 
     const response = await fetch(
       `https://getreporte-efud7l6bbq-uc.a.run.app?obraId=${obraId}&gremioId=${gremioId}&gremioName=${gremioName}`,
